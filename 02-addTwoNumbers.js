a9f5d114cc66ce80dd7d875c89d2309f90e632c9
Andrew Bliss <andrewgbliss@gmail.com>
2022-06-16 17:59:08 -0600

    updates

---
+++ 02-addTwoNumbers.js
@@ -0,0 +1,63 @@
+// https://leetcode.com/problems/add-two-numbers/
+
+// You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+// You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+ 
+
+// Example 1:
+
+
+// Input: l1 = [2,4,3], l2 = [5,6,4]
+// Output: [7,0,8]
+// Explanation: 342 + 465 = 807.
+// Example 2:
+
+// Input: l1 = [0], l2 = [0]
+// Output: [0]
+// Example 3:
+
+// Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+// Output: [8,9,9,9,0,0,0,1]
+ 
+
+// Constraints:
+
+// The number of nodes in each linked list is in the range [1, 100].
+// 0 <= Node.val <= 9
+// It is guaranteed that the list represents a number that does not have leading zeros.
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function (l1, l2) {
+	var dummyHead = new ListNode();
+	var curr = dummyHead;
+	var carry = 0;
+	var p = l1;
+	var q = l2;
+	while (p !== null || q !== null) {
+		var x = (p !== null) ? p.val : 0;
+		var y = (q !== null) ? q.val : 0;
+		var sum = x + y + carry;
+		carry = sum / 10;
+		curr.next = new ListNode(sum % 10);
+		curr = curr.next;
+		if (p !== null) p = p.next;
+		if (q !== null) q = q.next;
+	}
+	if (carry > 0) {
+		curr.next = new ListNode(carry);
+	}
+	return dummyHead.next;
+};
