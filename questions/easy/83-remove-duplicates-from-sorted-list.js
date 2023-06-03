bbb718be1a60e25faf1dbc595e181b74f5ca987e
Andrew Bliss <andrewgbliss@gmail.com>
2023-06-02 18:23:51 -0600

    updates

---
+++ questions/easy/83-remove-duplicates-from-sorted-list.js
@@ -0,0 +1,81 @@
+// https://leetcode.com/problems/remove-duplicates-from-sorted-list/description/
+
+// 83. Remove Duplicates from Sorted List
+// Easy
+// 7.3K
+// 249
+// Companies
+// Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+
+ 
+
+// Example 1:
+
+
+// Input: head = [1,1,2]
+// Output: [1,2]
+// Example 2:
+
+
+// Input: head = [1,1,2,3,3]
+// Output: [1,2,3]
+ 
+
+// Constraints:
+
+// The number of nodes in the list is in the range [0, 300].
+// -100 <= Node.val <= 100
+// The list is guaranteed to be sorted in ascending order.
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+    let curr = head;
+    while (curr) {
+        if (curr.next) {
+            let next = curr.next;
+            while (curr && next && curr.val === next.val) {
+                if (next.next) {
+                    curr.next = next.next;
+                } else {
+                    curr.next = null;
+                }
+                next = next.next;
+            }
+        }
+        curr = curr.next;
+    }
+    return head;
+};
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+    let curr = head;
+    while (curr && curr.next) {
+        if (curr.val === curr.next.val) {
+            curr.next = curr.next.next;
+            continue;
+        }
+        curr = curr.next;
+    }
+    return head;
+};
